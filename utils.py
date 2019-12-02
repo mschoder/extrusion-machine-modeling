5414eaeb8bb7ca5b76a86e372f5c8040027edd83
mschoder <mschoder@gmail.com>
2019-12-02 00:14:16 -0500

    features and autocorr

---
+++ utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+
+def crosscorr(datax, datay, lag=0, wrap=False):
+    """ Lag-N cross correlation. 
+    Shifted data filled with NaNs 
+    
+    Parameters
+    ----------
+    lag : int, default 0
+    datax, datay : pandas.Series objects of equal length
+    Returns
+    ----------
+    crosscorr : float
+    """
+    if wrap:
+        shiftedy = datay.shift(lag)
+        shiftedy.iloc[:lag] = datay.iloc[-lag:].values
+        return datax.corr(shiftedy)
+    else: 
+        return datax.corr(datay.shift(lag))
